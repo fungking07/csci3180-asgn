a4794efd04adf255970fcc755fdaafcd586c4587
Lam King Fung <lamkingfung@gmail.com>
2020-01-18 13:12:56 +0800

    fopen added

--- asgn1/asgn1.c
+++ asgn1/asgn1.c
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 
 int main(int argc, char const *argv[]) {
-  
+  FILE *fcan,*fins, *fout;
+  fcan = fopen("candidates.txt", "r");
+  fins = fopen("instructors.txt", "r");
   return 0;
 }

c40ec20fb742d4c92b95433ec9ac5a97694de326
BuildTools <lamkingfung@gmail.com>
2020-02-06 17:49:07 +0800

    Update ta_ranking.c

--- asgn1/ta_ranking.c
+++ asgn1/ta_ranking.c
@@ -20,21 +20,62 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+void course(FILE *fins, FILE *fcan);
+
 int main(int argc, char const *argv[]) {
   /*
     File I/O
   */
-  FILE *fcan,*fins, *fout;
+  FILE *fcan,*fins;
   fcan = fopen("candidates.txt", "r");
   fins = fopen("instructors.txt", "r");
+  /*
+    check if the file exist
+  */
   if(fcan == NULL || fins == NULL){
     printf("non-existing file!\n");
+    fclose(fcan);
+    fclose(fins);
     exit(1);
   }
-  fout = fopen("output.txt","wb");
+  course(fins, fcan);
   return 0;
 }
 
-char course(FILE *fins, FILE *fcan, FILE *fout){
-  
+void course(FILE *fins, FILE *fcan){
+  FILE *fout;
+  /*
+    check if instructor.txt is empty
+  */
+  int inssize;
+  fseek(fins, 0, SEEK_END);
+  inssize = ftell(fins);
+  if (inssize == 0) {
+    printf("instructors.txt is empty\n");
+    fout = fopen("output.txt", "wb");
+    fprintf(fout,"");
+    fclose(fcan);
+    fclose(fins);
+    exit(1);
+  }
+
+  /*
+    check if candidate.txt is empty
+  */
+  int cansize;
+  fseek(fcan, 0, SEEK_END);
+  cansize = ftell(fcan);
+  if(cansize == 0){
+    printf("candidates.txt is empty\n");
+    char codebuffer[127];
+    while(fgets(codebuffer, 127, fins) != NULL){
+      printf("there is content!\n");
+      printf("%s",codebuffer);
+    }
+    printf("wrong code!\n");
+    fclose(fcan);
+    fclose(fins);
+    exit(1);
+  }
+  printf("Both file not empty!\n");
 }
